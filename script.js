eeffda5525f4b0c79d0c1ba4fb2005e22f215b73
Bharat-Deora <bharadeora.39@gmail.com>
2020-07-03 17:12:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,10 +58,18 @@ function convert_Seconds(s){
 	var answer = "", comma = ", ", flag =0;
            
      
-    if(sec<min){
+    if(sec<min && sec>=0){
     	if(sec == 1){  answer += sec + " second";   }
         else if(sec ==0){  answer+="Now";  }
         else{  answer += sec + " seconds";  }
+    }
+    
+    else if(sec<0){
+	answer = "Time cannot be Reversed!";     
+    }
+	
+    else if(sec.includes('e')){
+        answer = "Please remove 'e' character!";
     }
 
     else if(sec<hyear){
