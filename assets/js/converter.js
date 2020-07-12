d0509f05c03b35b7dd170336cde00b560f5e586d
Bharat-Deora <bharadeora.39@gmail.com>
2020-07-12 23:49:06 +0530

    Update converter.js

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -93,6 +93,7 @@ function convert_Seconds(s){
 
 var swidth = screen.width;
 var banner = !!document.getElementById("banner");
+console.log(banner, banner.length);
 function hide(){
   if (swidth <= 500) {
   	if(banner.length > 0){
