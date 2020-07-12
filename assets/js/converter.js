ea4ee2c794891e4cfecbd9d8a36723fc02fcef70
Bharat-Deora <bharadeora.39@gmail.com>
2020-07-12 23:53:15 +0530

    Update converter.js

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -4,7 +4,7 @@ var child = document.getElementById("answer");
 function getValue(){
 	var  sec = document.getElementById("sec").value;
 	if (swidth <= 500) {
-	  if(!banner){ 
+	  if(!cbanner){ 
      	addElement('whole' , 'a', 'banner');
      }
     }
@@ -92,11 +92,12 @@ function convert_Seconds(s){
 
 
 var swidth = screen.width;
-var banner = !!document.getElementById("banner");
+var cbanner = !!document.getElementById("banner");
+var banner = document.getElementById("banner");
 console.log(banner, banner.length);
 function hide(){
   if (swidth <= 500) {
-  	if(banner){
+  	if(cbanner){
        banner.remove();
   	}
    } 
